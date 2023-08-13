f6019d08f7399aab08d82cd84e6979f2d493f385
Dustin <135719141+Dustin-thirdweb@users.noreply.github.com>
2023-08-13 18:50:50 -0500

    Create Main.tsx

---
+++ src/Main.tsx
@@ -0,0 +1,55 @@
+// disable es-lint
+import "./polyfills";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { XMTPProvider } from "@xmtp/react-sdk";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet } from "wagmi/chains";
+import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from "wagmi/providers/public";
+import App from "./controllers/AppController";
+import { isAppEnvDemo } from "./helpers";
+import { mockConnector } from "./helpers/mockConnector";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet],
+  [
+    infuraProvider({ apiKey: import.meta.env.VITE_INFURA_ID ?? "" }),
+    publicProvider(),
+  ],
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "thirdweb support chat",
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  chains,
+});
+
+const wagmiDemoClient = createClient({
+  autoConnect: true,
+  connectors: [mockConnector],
+  provider,
+  webSocketProvider,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+});
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <WagmiConfig client={isAppEnvDemo() ? wagmiDemoClient : wagmiClient}>
+    <RainbowKitProvider chains={chains}>
+      <StrictMode>
+        <XMTPProvider>
+          <App />
+        </XMTPProvider>
+      </StrictMode>
+    </RainbowKitProvider>
+  </WagmiConfig>,
+);
